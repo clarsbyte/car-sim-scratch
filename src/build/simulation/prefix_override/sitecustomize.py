ea39be15969fd6267f42efe197be4cf45a8cc5c0
clarsbyte <csaputra@ucsd.edu>
2025-11-27 01:25:05 -0800

    First Commit - Sim Done

--- src/build/simulation/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/clarissa/Documents/pers-robotics/car-scratch/src/install/simulation'
